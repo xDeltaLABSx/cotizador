f2872f4719cd61882df413d244e37f6e10c651d1
xDeltaLABSx <xrojox@gmail.com>
2026-08-02 22:07:17 -0600

    Remove commented function call in app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-cargar_plantillas_iniciales()# app.py
 import streamlit as st
 from config.settings import COMPANY_INFO
 from models.client_model import init_client_db
@@ -6,6 +5,7 @@ from views.quote_builder import render_quote_builder
 from views.template_manager import render_template_manager
 from views.client_directory import render_client_directory
 from services.plantillas import cargar_plantillas_iniciales
+
 
 # ==========================================
 # 2. CONFIGURACIÓN DE LA PÁGINA
